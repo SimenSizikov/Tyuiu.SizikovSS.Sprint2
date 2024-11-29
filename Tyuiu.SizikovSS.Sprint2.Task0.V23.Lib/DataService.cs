bf50e65d8d11b46c6262db5993cc36f504e6ab21
Сизиков Семён <Tyuiu.SizikovSS@outlook.com>
2024-11-29 22:30:02 +0500

    Task0

--- Tyuiu.SizikovSS.Sprint2.Task0.V23.Lib/DataService.cs
+++ Tyuiu.SizikovSS.Sprint2.Task0.V23.Lib/DataService.cs
@@ -13,10 +13,10 @@ namespace Tyuiu.SizikovSS.Sprint2.Task0.V23.Lib
         public bool[] GetCompareOperations(int x, int y)
         {
             bool[] res = new bool[6];
-            res[0] = x == y;
-            res[1] = x != y;
-            res[2] = y < x;
-            res[3] = x > y;
+            res[0] = x != y;
+            res[1] = x == y;
+            res[2] = x < y;
+            res[3] = y > x;
             res[4] = y <= x;
             res[5] = y >= x;
 
