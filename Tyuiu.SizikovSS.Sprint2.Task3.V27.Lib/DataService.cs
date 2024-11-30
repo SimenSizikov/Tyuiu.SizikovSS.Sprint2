4ba541489c50135fff2196251e2ccbe4763fe2c6
Сизиков Семён <Tyuiu.SizikovSS@outlook.com>
2024-11-30 11:35:15 +0500

    Task3

---
+++ Tyuiu.SizikovSS.Sprint2.Task3.V27.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.SizikovSS.Sprint2.Task3.V27.Lib
+{
+    public class DataService : ISprint2Task3V27
+    {
+        public double Calculate(double x)
+        {
+            if (x > 0) return Math.Round(x * Math.Pow(((x + 1) / (Math.Sin(Math.Pow(x, 2)) + x - 0.5)), x) ,3);
+            else if (x == 0) return Math.Round((Math.Pow(x, 2) - Math.Cos(Math.Pow(x, 2)) + 4) / (Math.Pow(x, 2) - Math.Sin(Math.Pow(x, 2)) + 12),3);
+            else if ((x > -31) && (x < 0)) return Math.Round(Math.Pow(1 + (1 / Math.Pow(x, 2)), 2), 3);
+            else return Math.Round(x + Math.Sin(Math.Pow(x, 5)) + Math.Pow(x, 3) - (2 / x) , 3);
+        }
+    }
+}
---
+++ Tyuiu.SizikovSS.Sprint2.Task3.V27.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SizikovSS.Sprint2.Task3.V27.Lib;
+
+namespace Tyuiu.SizikovSS.Sprint2.Task3.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new();
+            double x = 0, res = ds.Calculate(x);
+
+            Assert.AreEqual(0.0, res);
+        }
+    }
+}
