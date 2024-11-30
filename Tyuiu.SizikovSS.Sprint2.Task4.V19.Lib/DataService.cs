02bdc06efcbc570195988799e2a1646b7769f59a
Сизиков Семён <Tyuiu.SizikovSS@outlook.com>
2024-11-30 20:50:49 +0500

    Task4

--- Tyuiu.SizikovSS.Sprint2.Task4.V19.Lib/DataService.cs
+++ Tyuiu.SizikovSS.Sprint2.Task4.V19.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.SizikovSS.Sprint2.Task4.V19.Lib
     {
         public double Calculate(double x, double y)
         {
-            double z = x/2<y-6 ? Math.Round(Math.Pow(6+(4/Math.Pow(y,2)),x),3) : Math.Round((Math.Pow(x,2) - Math.Pow(Math.Cos(y),2) +10)/ (Math.Pow(y, 2) - Math.Pow(Math.Sin(y), 2) + 12),3);
+            double z = x/2<y-6 ? Math.Pow(6+(4/Math.Pow(y,2)),x) : (Math.Pow(x,2) - Math.Pow(Math.Cos(y),2) +10)/ (Math.Pow(y, 2) - Math.Pow(Math.Sin(y), 2) + 12);
             return z;
         }
     }
